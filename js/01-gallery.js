fac49a23f679265deca05370abdaf7eb590c78a8
AniaGoodness <aniagoodness@gmail.com>
2023-03-14 21:49:23 +0100

    galleryItems basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const imageGallery = document.querySelector('.gallery');
+console.log(imageGallery);
+const galleryItem = galleryItems.map(image => `<a class="gallery_link" href="${image.original}">
+<img class="gallery_image" src= "${image.preview}" data-source= "${image.original}" alt="${image.description}"></a>`).join("");
+imageGallery.insertAdjacentHTML("beforeend", galleryItem);
+console.log(galleryItem);
