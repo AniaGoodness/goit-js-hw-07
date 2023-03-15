f2fcbf03142b626c3d76e340f90685c20a681328
AniaGoodness <aniagoodness@gmail.com>
2023-03-15 20:48:01 +0100

    created gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,3 +6,18 @@ const galleryItem = galleryItems.map(image => `<a class="gallery_link" href="${i
 <img class="gallery_image" src= "${image.preview}" data-source= "${image.original}" alt="${image.description}"></a>`).join("");
 imageGallery.insertAdjacentHTML("beforeend", galleryItem);
 console.log(galleryItem);
+
+imageGallery.addEventListener("click", (event) => {
+    event.preventDefault();
+});
+
+/*function selectImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMAGE") {
+        return;
+    }
+    const selectedImage = event.target.dataset.original;
+    galleryItem.src = `${image.original}`;
+}*/
+
+    
