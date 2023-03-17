6024668adb3e2a0329e0071cd776eff148f28309
AniaGoodness <aniagoodness@gmail.com>
2023-03-17 11:36:17 +0100

    showOriginalImage

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+
 const imageGallery = document.querySelector('.gallery');
 console.log(imageGallery);
 
 const galleryElements = galleryItems.map((item) => 
     `<div class="gallery__item">
         <a class="gallery__link" href="${item.original}">
-            <img class="gallery_image" src= "${item.preview}" data-source= "${item.original}" alt="${item.description}"/>
+            <img class="gallery__image" 
+            src= "${item.preview}" 
+            data-source= "${item.original}" 
+            alt="${item.description}"/>
             </a>
             </div>`).join("");
 
 imageGallery.insertAdjacentHTML("beforeend", galleryElements);
 
-
-imageGallery.addEventListener("click", (event) => {
+const showOriginalImage = (event) => {
     event.preventDefault();
-});
-
-/*function selectImage(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMAGE") {
+        if (event.target.nodeName !== "IMAGE") {
         return;
     }
-    const selectedImage = event.target.dataset.original;
-    galleryItem.src = `${image.original}`;
+    const showOriginalImage = event.target.dataset.original;
+    galleryElements.src = `${image.original}`;
+
+}
+
+imageGallery.addEventListener("click", showOriginalImage);
+
+/*
+    const showOriginalImage = event.target.dataset.original;
+    Elements.src = `${image.original}`;
 }*/
 
     
